a83417f367c35485b76f79b37a623b0f5f3fbf97
Andrew Robbins <andrew@robbinsa.me>
2023-04-13 13:40:59 -0400

    Replace assert with thrown exception

--- common/nmf.hpp
+++ common/nmf.hpp
@@ -205,7 +205,17 @@ class NMF {
 
   NMF(const T &input, const AMAT &leftlowrankfactor,
       const AMAT &rightlowrankfactor): A(input) {
-    assert(leftlowrankfactor.n_cols == rightlowrankfactor.n_cols);
+    try
+    {
+      if (!(leftlowrankfactor.n_cols == rightlowrankfactor.n_cols))
+      {
+        throw std::logic_error("received factor matrices with uneven shape");
+      };
+    }
+    catch(const std::logic_error& e)
+    {
+      throw;
+    }
     // this->A = input;
     this->W = leftlowrankfactor;
     this->H = rightlowrankfactor;
@@ -387,7 +397,7 @@ class NMF {
     double err_time = toc();
 #ifdef _VERBOSE
     INFO << "err compute time::" << err_time << std::endl;
-#endif    
+#endif
     this->objective_err = arma::sum(splitErr);
   }
 
