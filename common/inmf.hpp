ea93e9f0daf1d3351c7e22fa917972e32173ed35
Andrew Robbins <andrew@robbinsa.me>
2023-05-25 13:10:58 -0400

    fixed member function calls

--- common/inmf.hpp
+++ common/inmf.hpp
@@ -5,6 +5,7 @@
 #endif
 #include <vector>
 #include <memory>
+#include "utils.hpp"
 
 namespace planc {
 
@@ -18,9 +19,9 @@ namespace planc {
         std::vector<std::unique_ptr<arma::mat>> Hi;  // each of size n_ixk
         std::vector<std::unique_ptr<arma::mat>> Vi;  // each of size mxk
         std::unique_ptr<arma::mat> W;                // mxk
-        const double lambda, sqrtLambda;
+        double lambda, sqrtLambda;
         std::vector<arma::mat> C_solveH;//(2*m, k);
-        const T B_solveH;//(2*m, n);
+        T B_solveH;//(2*m, n);
         std::vector<arma::mat> C_solveV;//(2*n, k);
         T B_solveV;//(2*n, m);
 
@@ -31,18 +32,18 @@ namespace planc {
         arma::mat B_solveW_i;   //(this->n, this->m); /// At(nxm) - HV(nxm);
 #endif
         void updateC_solveH(int i) {
-            arma::mat* Wptr = get(W);
-            arma::mat* Vptr = get(Vi[i]);
-            this->C_solveH[i] = arma::join_cols(&Wptr + &Vptr, sqrtLambda * &Vptr);
+            arma::mat* Wptr = W.get();
+            arma::mat* Vptr = Vi[i].get();
+            this->C_solveH[i] = arma::join_cols(*Wptr + *Vptr, sqrtLambda * *Vptr);
         }
 
         void updateC_solveV(int i) {
-            arma::mat* Hptr = get(Hi[i]);
-            this->C_solveV[i] = arma::join_cols(&Hptr, sqrtLambda * &Hptr);
+            arma::mat* Hptr = Hi[i].get();
+            this->C_solveV[i] = arma::join_cols(*Hptr, sqrtLambda * *Hptr);
         }
 
         T makeB_solveH(int i) { // call in constructor
-            T* Eptr = get(Ei[i]);
+            T* Eptr = Ei[i].get();
             arma::sp_mat B_H(2 * this->m, this->ncol_E[i]);
             // Copy the values from E to the top half of B_H
             B_H.rows(0, this->m) = &Eptr;
@@ -50,19 +51,25 @@ namespace planc {
         }
 
         void updateB_solveV(int i) {
-            T* Eptr = get(Ei[i]);
-            arma::mat* Wptr = get(W);
-            arma::mat* Hptr = get(Hi[i]);
+            T* Eptr = Ei[i].get();
+            arma::mat* Wptr = W.get();
+            arma::mat* Hptr = Hi[i].get();
             this->B_solveV = arma::zeros(2 * this->ncol_E[i], this->m);
-            this->B_solveV.rows(0, this->ncol_E[i] - 1) = &Eptr.t() - &Hptr * &Wptr.t();
+            this->B_solveV.rows(0, this->ncol_E[i] - 1) = Eptr->t() - *Hptr * Wptr->t();
         }
     public:
-        INMF(std::vector<std::unique_ptr<T>> Ei,
+        INMF(std::vector<std::unique_ptr<T>> &Ei,
              arma::uword k) {
-            this->Ei = Ei
+            Ei.swap(this->Ei);
             this->k = k;
-            this->m = 0; // TODO
-            //this->ncol_E = ; // TODO
+            this->m = Ei[0]->n_rows;
+            for (typename std::vector<std::unique_ptr<T>>::iterator it = Ei.begin(); it != Ei.end(); ++it)
+            {
+                arma::mat* E = it->get();
+                this->ncol_E.push_back(E->n_cols);
+            };
+            this->lambda = 0; //TODO
+            this->sqrtLambda = 0; //TODO
             //TODO implement
         }
     };
---
+++ nmf/inmf.cpp
@@ -0,0 +1,13 @@
+#include "../common/inmf.hpp"
+#include "../common/utils.hpp"
+#include <memory>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+	arma::mat tempMat = arma::zeros(10, 10);
+	arma::mat* tempMatptr = &tempMat;
+	std::unique_ptr<arma::mat> testPtr = std::unique_ptr<arma::mat>(tempMatptr);
+	std::vector<std::unique_ptr<arma::mat>> ptrvec;
+	ptrvec.push_back(std::move(testPtr));
+	planc::INMF<arma::mat> testINMF = planc::INMF<arma::mat>(ptrvec, 10);
+}
