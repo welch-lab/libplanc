11ce9d49c4c100eb5a6c84a87f63aa1fe7cc1eaa
Andrew Robbins <andrew@robbinsa.me>
2024-06-19 19:41:23 -0400

    explicit intantiation of EXPORT

--- nmf/nmf_lib.hpp
+++ nmf/nmf_lib.hpp
@@ -37,13 +37,27 @@ namespace planc {
     //extern std::map<std::string, runNMFindex> NMFindexmap;
 
     template <typename T>
-    nmfOutput NMFLIB_EXPORT nmf(const T &x,
+    nmfOutput NMFLIB_NO_EXPORT nmf(const T &x,
                                 const arma::uword &k,
                                 const arma::uword &niter = 30,
                                 const std::string &algo = "anlsbpp",
                                 const int &nCores = 2,
                                 const arma::mat &Winit = arma::mat(),
                                 const arma::mat &Hinit = arma::mat());
+    template<>  nmfOutput NMFLIB_EXPORT nmf<arma::mat>(const arma::mat &x,
+                                                  const arma::uword &k,
+                                                  const arma::uword &niter,
+                                                  const std::string &algo,
+                                                  const int &nCores,
+                                                  const arma::mat &Winit,
+                                                  const arma::mat &Hinit);
+    template<>  nmfOutput NMFLIB_EXPORT nmf<arma::sp_mat>(const arma::sp_mat &x,
+                                                       const arma::uword &k,
+                                                       const arma::uword &niter,
+                                                       const std::string &algo,
+                                                       const int &nCores,
+                                                       const arma::mat &Winit,
+                                                       const arma::mat &Hinit);
 }
 
 #endif //PLANC_NMF_LIB_HPP
