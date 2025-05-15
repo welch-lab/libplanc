d0f82e0fa5a7f19d9260ea40e611dbd6c0263011
Andrew Robbins <andrew@robbinsa.me>
2025-05-15 14:26:35 -0400

    try using python-backed wrapper for progress

---
+++ python/progressWrapper.h
@@ -0,0 +1,36 @@
+//
+// Created by andrew on 11/28/2023.
+//
+
+#ifndef PLANC_PROGRESSWRAPPER_H
+#define PLANC_PROGRESSWRAPPER_H
+
+#include <cstdint>
+#include <nanobind/nanobind.h>
+
+
+namespace nb = nanobind;
+
+class Progress {
+private:
+    nanobind::object wrappedbar;
+    nanobind::callable wrappedbartick;
+    int i_;
+public:
+    explicit Progress(unsigned long max, bool display_progress = true) {
+        if (display_progress) {
+            nb::module_ progressbar = nb::module_::import_("progressbar");
+            nb::callable wrappedbarGen = progressbar.attr("progressbar");
+            wrappedbar = wrappedbarGen(max);
+            wrappedbartick = wrappedbar.attr("update");
+            i_ = 0;
+        }
+    };
+    void increment() {
+        wrappedbartick(i_);
+        i_++;
+    };
+};
+
+
+#endif //PLANC_PROGRESSWRAPPER_H
